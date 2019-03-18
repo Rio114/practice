b4b18ee01329ea4cd68ec626975e383c5f8b53ef
Rio114 <nomuraryoji114@gmail.com>
2019-03-18 23:14:31 +0900

    class DefaultBox

---
+++ python_ML/ssd_layer.py
@@ -0,0 +1,74 @@
+import numpy as np 
+import keras.backend as K
+from keras.engine.topology import Layer
+
+class DefaultBox(Layer):
+    def __init__(self, img_size, min_size, max_size, aspect_ratios, variance, **kwargs):
+        self.img_size = img_size
+        if min_size < 0:
+            raise Exception('min_size must be positive')
+        self.min_size = min_size
+        if max_size < min_size:
+            raise Exception('max_size must be grater than min_size')
+        self.max_size = max_size
+        self.aspect_ratios = aspect_ratios
+        self.variance = np.array(variance)
+        
+        super(DefaultBox, self).__init__(**kwargs)
+
+    def call(self, x, mask=None):
+        input_shape = K.int_shape(x)
+
+        layer_width = input_shape[1]
+        layer_height = input_shape[2]
+        
+        img_width = self.img_size[0]
+        img_height = self.img_size[1]
+
+        box_widths = []
+        box_heights = []
+        for ar in self.aspect_ratios:
+            if ar == 1 and len(box_widths) == 0:
+                box_widths.append(self.min_size)
+                box_heights.append(self.min_size)
+            elif ar == 1 and len(box_widths) > 0:
+                box_widths.append(np.sqrt(self.min_size * self.max_size))
+                box_heights.append(np.sqrt(self.min_size * self.max_size))
+            elif ar != 1:
+                box_widths.append(self.min_size * np.sqrt(ar))
+                box_heights.append(self.min_size / np.sqrt(ar))
+
+        box_widths = 0.5 * np.array(box_widths)
+        box_heights = 0.5 * np.array(box_heights)
+
+        step_x = img_width / layer_width
+        step_y = img_height / layer_height       
+        lin_x = np.linspace(0.5 * step_x, img_width - 0.5 * step_x, layer_width)
+        lin_y = np.linspace(0.5 * step_y, img_height - 0.5 * step_y, layer_height)
+        centers_x, centers_y = np.meshgrid(lin_x, lin_y)
+        centers_x = centers_x.reshape(-1, 1)
+        centers_y = centers_y.reshape(-1, 1)
+
+        num_defaults = len(self.aspect_ratios)
+        default_boxes = np.concatenate((centers_x, centers_y), axis=1)
+        default_boxes = np.tile(default_boxes, (1, 2 * num_defaults))
+        default_boxes[:, ::4] -= box_widths
+        default_boxes[:, 1::4] -= box_heights
+        default_boxes[:, 2::4] += box_widths
+        default_boxes[:, 3::4] += box_heights
+        default_boxes[:, ::2] /= img_width
+        default_boxes[:, 1::2] /= img_height
+        default_boxes = default_boxes.reshape(-1, 4)
+        num_boxes = len(default_boxes)
+        variance = np.ones((num_boxes, 4)) * self.variance
+        default_boxes = np.concatenate((default_boxes, variance), axis=1)
+        default_boxes_tensor = K.expand_dims(K.variable(default_boxes), 0)
+        
+        return default_boxes_tensor
+    
+
+
+                
+
+
+
