c4c2809fb5c88f35205a534c802bfb88d907c8aa
Rio114 <nomuraryoji114@gmail.com>
2020-03-15 20:53:16 +0900

    wip

--- cpp/aizu/05ch/binarySearch.cpp
+++ cpp/aizu/05ch/binarySearch.cpp
@@ -2,3 +2,40 @@
 #include<vector>
 #include<iostream>
 using namespace std;
+
+int search(vector<int> a, int n, int key){
+    int left = 0; 
+    int right = n; 
+
+    while(left < right){
+        int mid = (left + right ) /2;
+        if(a.at(mid) == key){
+            return mid;
+        }else if (key < a.at(mid)){
+            right = mid;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int n, i, q, key, cnt = 0;
+    cin >> n;
+    vector<int> a;
+    for(int i=0; i<n; i++){
+        int b;
+        std::cin >> b;
+        a.push_back(b);
+    }
+
+    cin >> q;
+    
+    for(int i=0; i<q; i++){
+        cin >> key;
+        if( search(a, n, key) ) cnt++;
+    }
+    
+    cout << cnt << endl;    
+}
