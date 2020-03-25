206c7346e74fd32d75cbb9e39a74689be70e8ef0
Rio114 <nomuraryoji114@gmail.com>
2020-03-26 08:36:37 +0900

    wip

--- cpp/aizu/07ch/quickSort.cpp
+++ cpp/aizu/07ch/quickSort.cpp
@@ -34,6 +34,55 @@ void mergeSort(struct Card A[], int n, int left, int right){
     }
 }
 
-int partition(struct Card A[], int n, int q, int r){
+int partition(struct Card A[], int n, int p, int r){
+    int i, j;
+    struct Card t, x;
+    x = A[r];
+    i = p-1;
+    for(j = p; j<r; j++){
+        if(A[j].value <= x.value){
+            i++;
+            t = A[i]; A[i] = A[j]; A[j] = t; 
+        }
+    }
+    t = A[i+1]; A[i+1] = A[r]; A[r] = t;
+    return i+1;
+}
+
+void quickSort(struct Card A[], int n, int p, int r){
+    int q;
+    if(p < r) {
+        q = partition(A, n, p, r);
+        quickSort(A, n, p, q-1);
+        quickSort(A, n, q+1, r);
+    }
+}
+
+int main(){
+    int n, i, v;
+    struct Card A[MAX], B[MAX];
+    char S[10];
+    int stable = 1;
+
+    scanf("%d", &n);
+
+    for (i=0; i<n; i++){
+        scanf("%s %d", S, &v);
+        A[i].suit = B[i].suit = S[0];
+        A[i].value = B[i].value = v;
+
+    }
+
+    mergeSort(A, n, 0, n);
+    quickSort(B, n, 0, n-1);
     
+    for (i=0; i<n; i++){
+        if(A[i].suit != B[i].suit) stable = 0;
+    }
+
+    if(stable == 1) printf("Stable\n");
+    else printf("Not stable\n");
+    for(i = 0; i<n; i++){
+        printf("%c %d\n", B[i].suit, B[i].value);
+    }
 }
