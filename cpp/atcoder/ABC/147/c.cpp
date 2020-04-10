075efaf9a810c28cd508c98d20675dc20c2d63f6
Rio114 <nomuraryoji114@gmail.com>
2020-04-10 19:31:04 +0900

    wip

--- cpp/atcoder/ABC/147/c.cpp
+++ cpp/atcoder/ABC/147/c.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
+#include<bitset>
 using namespace std;
+#define MAX 15
+
 
 int main(){
     int i, n;
@@ -14,9 +17,22 @@ int main(){
         }
     }
 
-    while(1){
-        
+    int cnt, max_cnt=0;
+    for (int tmp=0; tmp<(1<<n); tmp++){
+        bitset<MAX> hyp(tmp);
+        cnt = 0;        
+        bool flg=true;
+        for(i=0; i<n; i++){
+            if(hyp.test(i)){
+                cnt++;
+                for(int j=0; j<a[i]; j++){
+                    if(hyp.test(x[i][j]) != y[i][j]) {
+                        flg = false;
+                    }
+                }
+            }
+        }
+        if(flg == true) max_cnt = max(max_cnt, cnt);
     }
-
-
+    cout << max_cnt << endl;
 }
