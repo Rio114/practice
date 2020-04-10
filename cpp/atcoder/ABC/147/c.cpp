aed89924df920e3c5b88cc7221067a9612330061
Rio114 <nomuraryoji114@gmail.com>
2020-04-10 22:54:36 +0900

    147

--- cpp/atcoder/ABC/147/c.cpp
+++ cpp/atcoder/ABC/147/c.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 #define MAX 15
 
-
 int main(){
     int i, n;
     cin >> n;
-    int hon=n;
-    int a[n];
-    int x[n][n], y[n][n];
+    int a[n], x[n][n], y[n][n];
     for(i=0; i<n; i++){
         cin >> a[i];
         for(int j=0; j<a[i]; j++){
@@ -22,17 +19,19 @@ int main(){
         bitset<MAX> hyp(tmp);
         cnt = 0;        
         bool flg=true;
+        // cout << hyp << endl;
         for(i=0; i<n; i++){
             if(hyp.test(i)){
                 cnt++;
                 for(int j=0; j<a[i]; j++){
-                    if(hyp.test(x[i][j]) != y[i][j]) {
+                    if(hyp.test(x[i][j]-1) != y[i][j]) {
                         flg = false;
                     }
                 }
             }
         }
         if(flg == true) max_cnt = max(max_cnt, cnt);
+        // cout << flg << " " << cnt << endl;
     }
     cout << max_cnt << endl;
 }
