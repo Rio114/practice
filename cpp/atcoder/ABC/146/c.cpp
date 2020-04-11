3b2086497b24d90f5e6495f777327eb0dbdf2046
Rio114 <nomuraryoji114@gmail.com>
2020-04-11 22:46:41 +0900

    146

--- cpp/atcoder/ABC/146/c.cpp
+++ cpp/atcoder/ABC/146/c.cpp
@@ -2,35 +2,32 @@
 #include<cmath>
 using namespace std;
 typedef long long llong;
-#define NMAX 1000000000
+#define NMAX 1000000001
 
 int main(){
     llong a, b, x;
     cin >> a >> b >> x;
 
-    llong dmax = log10(NMAX) + 1;
-    llong pmax = a * NMAX + b* dmax;
-    if(pmax <= x){
-        cout << NMAX <<  endl;
-        return 0;
-    }    
+    // llong dmax = log10(NMAX) + 1;
+    // llong pmax = a * NMAX + b * dmax;
+    // if(pmax <= x){
+    //     cout << NMAX <<  endl;
+    //     return 0;
+    // }    
 
-    llong p, d, n=1, nmax=NMAX-1, nmin=1;
-    while(1){
+    llong p, d, n=1, r=NMAX, l=0;
+    while(abs(r-l) > 1){
+        n = (l + r) / 2;
         d = log10(n) + 1;
+        // d = to_string(n).length();
         p = a * n + b * d;
 
-        if(p < x){
-            nmin = n;
-            if(nmax - nmin == 1) break;
-            else n = (nmax + nmin) / 2 ;
-        }else{
-            nmax = n;
-            n = n / 2;
-            if(n < 1) {
-                break;
-            }
+        if(p <= x){
+            l = n;
+            n = (r + n) / 2;
+        } else{
+            r = n;
         }
     }
-    cout << n << endl;
+    cout << l << endl;
 }
