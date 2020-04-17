39de022d493a4f77b18dd0dccc99e66dc25d9820
Rio114 <nomuraryoji114@gmail.com>
2020-04-17 17:50:06 +0900

    wip

---
+++ cpp/atcoder/ABC/162/d.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+static const int MAX = 4000;
+
+int main(){
+    int n;
+    char s[MAX];
+    cin >> n;
+    cin >> s;
+
+    cout << s << endl;
+    int i, j, k;
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            for(k=0; k<n; k++){
+                
+            }
+        }
+    }
+
+}
