7261e14292d9c7e057a5da636906209c5dd031d1
Rio114 <nomuraryoji114@gmail.com>
2020-04-19 00:21:07 +0900

    162d

--- cpp/atcoder/ABC/162/d.cpp
+++ cpp/atcoder/ABC/162/d.cpp
@@ -1,27 +1,39 @@
 #include<iostream>
 using namespace std;
+typedef long long llong;
 
-static const int MAX = 4000;
+static const int MAX = 4001;
 
 int main(){
-    int n;
+    llong n;
     char s[MAX];
-    // scanf("%d", &n);
-    // scanf("%s", s);
-
     cin >> n;
     cin >> s;
 
-    int cnt = 0;
+    llong cnt = 0, r = 0, g = 0, b = 0;
     int i, j, k;
     for(i=0; i<n; i++){
-        for(j=i+1; j<n; j++){
-            for(k=j+1; k<n; k++){
-                if(s[i]!=s[j] && s[j]!=s[k] && s[k]!=s[i] && j-i!=k-j) cnt++;
-            }
+        if(s[i] == 'R') r++;
+        else if(s[i] == 'G') g++;
+        else if(s[i] == 'B') b++;
+    }
+
+    for(i=0; i<n-2; i++){
+        for(j=i+1; j<n-1; j++){
+            k = 2 * j - i;
+            if(s[i] != s[j] && s[k] != s[j] && s[k] != s[i] && k < n) cnt++;
         }
     }
 
-    // printf("%d\n", cnt);
-    cout << cnt << endl;
+    llong ans = r*g*b - cnt;
+    
+    // for(i=0; i<n; i++){
+    //     for(j=i+1; j<n; j++){
+    //         for(k=j+1; k<n; k++){
+    //             if(s[i]!=s[j] && s[j]!=s[k] && s[k]!=s[i] && j-i!=k-j) cnt++;
+    //         }
+    //     }
+    // }
+
+    cout << ans << endl;
 }
