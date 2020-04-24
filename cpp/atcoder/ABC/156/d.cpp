fa13646641acd57ed3742800e710d45f1b96effa
Rio114 <nomuraryoji114@gmail.com>
2020-04-24 19:21:23 +0900

    wip

--- cpp/atcoder/ABC/156/d.cpp
+++ cpp/atcoder/ABC/156/d.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-#include<bitset>
-#define MAX 10
+typedef long long llong;
+
+llong
+
 
 int main() {
     int n, a, b;
     cin >> n >> a >> b;
+    llong cnt = 0;
 
-    for (int tmp = 0; tmp < (1 << n); tmp++) {
-        bitset<MAX> s(tmp);
-        cout << s << endl;
+
+    for(int i=1; i<=n; i++){
+        if(i != a && i != b){
+            cnt += pow(2, i);
+        }
     }
+
+    cout << cnt << endl;
 }
