e7d465a0ab5721d8acbe4f3cd09ab88132b5a915
Rio114 <nomuraryoji114@gmail.com>
2020-05-03 07:36:15 +0900

    wip

--- opencv_py/run_tf_python.sh
+++ opencv_py/run_tf_python.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # docker run --rm -it -v $PWD:/work tf_opencv bash
-docker run --rm -v $(pwd):/tf/works -p 10000:8888 tensorflow/tensorflow:latest-gpu-py3-jupyter
+docker run -v $(pwd):/tf/works -p 10000:8888 keras:vgg16
+
 
