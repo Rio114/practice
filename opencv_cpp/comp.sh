a660bce9913c7633328b29e276b1fe398b0e1430
Rio0114 <nomuraryoji114@gmail.com>
2020-05-13 16:59:59 +0900

    sr3

--- opencv_cpp/comp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ $1 `pkg-config --cflags opencv` `pkg-config --libs opencv`
-
--- opencv_cpp/run_docker_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# docker run -it -v ~/Documents/git/practice/opencv_cpp:/opencv 17685f682a24 bash
-
-docker start thirsty_borg
-docker attach thirsty_borg 
-
