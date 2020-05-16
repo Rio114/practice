bfb9788eec2f3f5abf8f7926068837eb89b12447
Rio0114 <nomuraryoji114@gmail.com>
2020-05-16 16:01:35 +0900

    clean docker

--- tf_opencv/docker/setup.sh
+++ tf_opencv/docker/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # set up opencv-python in tf-docker
-# Because of area selection, exexuted after docker build
+# Because of area selection while install, it's exexuted after docker build
+# user shall be added
+
+usermod -u $USER_ID -o -m user
+groupmod -g $GROUP_ID user
 
 # opencv-devのインストール
 apt-get update -y
@@ -9,5 +13,7 @@ apt-get install -y libopencv-dev
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-# TensorflowとOpencvのインストール
+# adduser $USER
+
+# Opencvのインストール
 pip install opencv-python
--- tf_opencv/scripts/run_docker.sh
+++ tf_opencv/scripts/run_docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker run --gpus all -it -v $(pwd):/home/keras_opencv -p 8888:8888 tf2-opencv:gpu bash
+# for setup
+# docker run --gpus all -it --name gpu_temp -v $(pwd):/home/gpu -p 8888:8888 gpu:temp bash
 
+# to make image
+# docker commit gpu_temp gpu:opencv
+
+# after setup
+# docker run --gpus all -it -u $(id -u):$(id -g) --name gpu -v $(pwd):/home/gpu -p 8888:8888 gpu:opencv bash
+
+# for use
+docker start -i gpu
