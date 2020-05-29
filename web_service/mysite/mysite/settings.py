a3d9e2af1be5ebe841d1902f61ca8b10191960af
Rio0114 <nomuraryoji114@gmail.com>
2020-05-29 09:44:34 +0900

    hostname

--- web_service/mysite/mysite/settings.py
+++ web_service/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zhp*efie$&f-ton8r$qcrltxwnxo-(!yn13sj9ht-8mflbpxpm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.107', 'localhost']
 
 
 # Application definition
