ae39b843917b024863d669fb0dfb5e6dbdd666e6
Rio0114 <nomuraryoji114@gmail.com>
2020-06-10 22:38:22 +0900

    docker

--- tf_opencv/scripts/run_docker.sh
+++ tf_opencv/scripts/run_docker.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# for setup
-# docker run --gpus all -it --name gpu_temp -v $(pwd):/home/gpu -p 8888:8888 gpu:temp bash
-
-# to make image
-# docker commit gpu_temp gpu:opencv
-
-# after setup
-# docker run --gpus all -it -u $(id -u):$(id -g) --name gpu -v $(pwd):/home/gpu -p 8888:8888 gpu:opencv bash
+# docker run --gpus all -it -u $(id -u):$(id -g) --name keras-cv -v $(pwd):/home/work -p 8888:8888 keras-cv:latest
 
 # for network
 # docker network create srgan-service
 # docker run --gpus all --network srgan-service -it -u $(id -u):$(id -g) --name gpu -v $(pwd):/home/gpu -p 8888:8888 gpu:opencv bash
 
 # for use
-docker start -i gpu
+docker start -i keras-cv
