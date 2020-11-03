0b2e4851636fe85a6fcdd1c29e97dd0304eb051c
Rio114 <nomuraryoji114@gmail.com>
2020-11-03 14:39:51 +0900

    cycledetection

---
+++ kyopro/aizu/GRL/GRL_4_A_CycleDetection_war.py
@@ -0,0 +1,50 @@
+
+class war():
+    def __init__(self):
+        self.__read_data()
+        self.reachables = [[None] * self.V for _ in range(self.V)]
+        self.__assign_reashable()
+
+
+    def __read_data(self):
+        self.V, self.E = list(map(int, input().split()))
+        self.Nodes = [list(map(int, input().split())) for _ in range(self.E)]
+
+
+    def __assign_reashable(self):
+        for i in range(self.V):
+            for j in range(self.V):
+                self.reachables[i][j] = (i == j)
+
+        for node in self.Nodes:
+            self.reachables[node[0]][node[1]] = True
+
+    def __war(self):
+        for m in range(self.V):
+            for s in range(self.V):
+                if self.reachables[s][m] == False:
+                    continue
+                for t in range(self.V):
+                    if self.reachables[m][t] == False:
+                        continue
+                    self.reachables[s][t] = True
+
+        for s in range(self.V - 1):
+            for t in range(s+1, self.V):
+                if self.reachables[s][t] == True and self.reachables[t][s] == True:
+                    return 1
+
+        return 0
+
+
+    def exec(self):
+        print(self.__war())
+            
+
+def main():
+    obj = war()
+    obj.exec()
+
+
+if __name__ == "__main__":
+    main()
