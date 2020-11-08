0ff60556ea3dc95a8a5cf10c85509a7b0f8b74cd
Rio114 <nomuraryoji114@gmail.com>
2020-11-08 17:16:16 +0900

    height

--- kyopro/aizu/GRL/GRL_5_B_HeightOfATree_bfs.py
+++ kyopro/aizu/GRL/GRL_5_B_HeightOfATree_bfs.py
@@ -1,14 +1,13 @@
-
-INF = 1e+10
+import sys
+sys.setrecursionlimit(1000000)
 
 
 class TreeHeight():
     def __init__(self):
         self.__read_data()
         self.__assign_adj_list()
-        self.__assing_dist_mat()
-        self.preorder = []
-        self.postorder = []
+        self.max_dist = 0
+        self.max_id = -1
 
     def __read_data(self):
         self.N = int(input())
@@ -20,30 +19,27 @@ class TreeHeight():
             self.adj_list[node[0]].append([node[1], node[2]])
             self.adj_list[node[1]].append([node[0], node[2]])
 
-    def __assing_dist_mat(self):
-        self.dist_mat = [[INF for i in range(self.N)] for j in range(self.N)]
-        for i in range(self.N):
-            self.dist_mat[i][i] = 0
-
-    def __dfs(self, curr, prev):
-        self.preorder.append((curr, prev))
+    def __dfs(self, curr, prev, dists):
         for v, w in self.adj_list[curr]:
-            if v != prev:
-                self.__dfs(v, curr)
-        self.postorder.append(curr)
-
-    # def __find_max_dist(self):
-    #     for dists in self.dist_mat:
-    #         print(max(dists))
+            if v == prev:
+                continue
+            dists[v] = dists[curr] + w
+            if self.max_dist <= dists[v]:
+                self.max_dist = dists[v]
+                self.max_id = v
+            self.__dfs(v, curr, dists)
 
     def exec(self):
-        self.__dfs(0, None)
-        print(self.preorder)
-        print(self.postorder)
+        dists0 = [0 for _ in range(self.N)]
+        dists1 = [0 for _ in range(self.N)]
+        dists2 = [0 for _ in range(self.N)]
 
-        # for s in range(self.N):
-        #     self.__bfs_dist(s)
-        # self.__find_max_dist()
+        self.__dfs(0, None, dists0)
+        self.__dfs(self.max_id, None, dists1)
+        self.__dfs(self.max_id, None, dists2)
+
+        for i in range(self.N):
+            print(max(dists1[i], dists2[i]))
 
 
 def main():
