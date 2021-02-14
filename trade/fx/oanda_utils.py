b41661a117b390fa791315c832434d074ec51392
Rio114 <nomuraryoji114@gmail.com>
2021-02-14 09:58:27 +0900

    init oanda utils

---
+++ trade/fx/oanda_utils.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+
+from oandapyV20 import API
+import oandapyV20.endpoints.instruments as instruments
+
+accountID = os.environ["oanda_account_id"]
+access_token = os.environ["oanda_access_token"]
+api = API(access_token=access_token, environment="live")
+
+
+def get_candles_df(count, granularity, instrument):
+    params = {"count": count, "granularity": granularity}
+    r = instruments.InstrumentsCandles(instrument=instrument, params=params)
+    data = api.request(r)
+
+    records = []
+
+    for c in data["candles"]:
+        records.append(
+            [
+                c["time"],
+                c["complete"],
+                c["mid"]["o"],
+                c["mid"]["h"],
+                c["mid"]["l"],
+                c["mid"]["c"],
+                c["volume"],
+            ]
+        )
+
+    cols = ["time", "flg", "O", "H", "L", "C", "V"]
+    df = pd.DataFrame(records, columns=cols)
+    df["time"] = pd.to_datetime(df["time"])
+    df[["O", "H", "L", "C"]] = df[["O", "H", "L", "C"]].astype("float")
+    df["V"] = df["V"].astype("int")
+    df["flg"] = df["flg"].astype("bool")
+    return df
